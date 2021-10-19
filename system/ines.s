7e7b1fb346a7cb1856b5ed0f9f092db1191daa90
Sylvain Gadrat <sgadrat@wontfix.it>
2021-10-19 10:11:43 +0200

    Generate full chr-rom

--- system/ines.s
+++ system/ines.s
@@ -6,7 +6,7 @@ MAPPER_BATTERY_FLAG = 0
 
 .byte 'N', 'E', 'S', $1a ; ID
 .byt 32         ; PRG section occupies 32*16KiB memory
-.byt 1          ; CHR-ROM section occupies 1*8KiB memory
+.byt 64         ; CHR-ROM section occupies 64*8KiB memory
 .byt ((MAPPER_NUMBER & $00f) << 4) + (MAPPER_BATTERY_FLAG << 1) ; Flags 6 NNNN FTBM - mapper low nibble, no four screen, no trainer, persistent memory, horizontal mirroring
 .byt (MAPPER_NUMBER & $0f0) + %00001000 ; Flags 7 NNNN 10TT - mapper mid nibble, NES 2.0, NES/Famicom
 .byt (SUBMAPPER_NUMBER << 4) + ((MAPPER_NUMBER & $f00) >> 8) ; Flags 8 SSSS NNNN - submapper, mapper high nibble
